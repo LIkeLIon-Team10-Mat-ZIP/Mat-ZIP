e435bd837b5cc8502bd3d9f0444dbd6a6d1dcd45
JunTaeINC <juntea3523@naver.com>
2023-06-20 23:11:34 +0900

    docs: initData, ReviewServiceTest Frame 생성

---
+++ src/test/java/site/matzip/review/domain/service/ReviewServiceTest.java
@@ -0,0 +1,25 @@
+package site.matzip.review.domain.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class ReviewServiceTest {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @Test
+    @DisplayName("create() Test")
+    void createTest() {
+
+    }
+}
