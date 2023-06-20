199375a7b189af8b77a061005fc58064bd2dced3
JunTaeINC <juntea3523@naver.com>
2023-06-20 21:18:31 +0900

    feat: Review Entity 생성

---
+++ src/main/java/site/matzip/review/domain/Review.java
@@ -0,0 +1,27 @@
+package site.matzip.review.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import site.matzip.article.domain.Article;
+import site.matzip.base.domain.BaseEntity;
+import site.matzip.reviewImage.domain.ReviewImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+public class Review extends BaseEntity {
+    @ManyToOne
+    private Article article;
+    @OneToMany(mappedBy = "review", cascade = {CascadeType.ALL})
+    @Builder.Default
+    private List<ReviewImage> reviewImages = new ArrayList<>();
+    private Double rating;
+    private String content;
+}
