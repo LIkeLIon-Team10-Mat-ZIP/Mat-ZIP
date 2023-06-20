65532f35595a872fe7f0a80a6cf66b41dd39b3e6
JunTaeINC <juntea3523@naver.com>
2023-06-20 17:06:48 +0900

    feat: BaseEntity 생성

---
+++ src/main/java/site/matzip/base/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package site.matzip.base.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@SuperBuilder
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+}
