fee3d7059235a57201afa87297c2235c358258ef
heeyeon3050 <kho200a@naver.com>
2023-06-21 23:16:52 +0900

    feat: 마커 이미지 변경

--- src/main/java/site/matzip/base/domain/BaseEntity.java
+++ src/main/java/site/matzip/base/domain/BaseEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class BaseEntity {
     @CreatedDate
     private LocalDateTime createDate;
