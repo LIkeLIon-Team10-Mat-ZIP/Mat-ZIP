361b6a586008c92ac331fef4436bdf2628e81b74
JunTaeINC <juntea3523@naver.com>
2023-06-23 17:38:38 +0900

    feat: CommentService create() / DTO 수정

--- src/main/java/site/matzip/comment/dto/CommentCreationDTO.java
+++ src/main/java/site/matzip/comment/dto/CommentCreationDTO.java
@@ -5,7 +5,8 @@ import lombok.Data;
 
 @Data
 public class CommentCreationDTO {
-    private Long matzipId;
+    private Long authorId;
+    private Long reviewId;
     @NotBlank(message = "리뷰의 별점을 입력해 주세요!")
     private Double rating;
     @NotBlank(message = "리뷰의 제목을 입력해 주세요!")
--- src/main/java/site/matzip/comment/service/CommentService.java
+++ src/main/java/site/matzip/comment/service/CommentService.java
@@ -1,24 +1,37 @@
 package site.matzip.comment.service;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import site.matzip.comment.domain.Comment;
+import site.matzip.comment.dto.CommentCreationDTO;
 import site.matzip.comment.repository.CommentRepository;
+import site.matzip.member.domain.Member;
+import site.matzip.member.repository.MemberRepository;
 import site.matzip.review.domain.Review;
+import site.matzip.review.repository.ReviewRepository;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
 
-    public void create(Review review, Double rating, String title, String content) {
+    public void create(CommentCreationDTO commentCreationDTO) {
+        Member author = memberRepository.findById(commentCreationDTO.getAuthorId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        Review review = reviewRepository.findById(commentCreationDTO.getReviewId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Review not found"));
+
         Comment comment = Comment.builder()
+                .author(author)
                 .review(review)
-                .rating(rating)
-                .title(title)
-                .content(content)
+                .rating(commentCreationDTO.getRating())
+                .title(commentCreationDTO.getTitle())
+                .content(commentCreationDTO.getContent())
                 .build();
 
         commentRepository.save(comment);
