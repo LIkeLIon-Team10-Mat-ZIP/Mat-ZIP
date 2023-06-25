b6a4f39f1cc4e2e65b3c63f2f84327445fd1bc5e
JunTaeINC <juntea3523@naver.com>
2023-06-25 15:03:42 +0900

    feat: Review rating,reviewImage Field 추가

--- src/main/java/site/matzip/review/domain/Review.java
+++ src/main/java/site/matzip/review/domain/Review.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import site.matzip.base.domain.BaseEntity;
 import site.matzip.comment.domain.Comment;
 import site.matzip.matzip.domain.Matzip;
+import site.matzip.reviewImage.domain.ReviewImage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +19,13 @@ public class Review extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Double rating;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "matzip_id")
     private Matzip matzip;
     @OneToMany(mappedBy = "review", cascade = {CascadeType.ALL})
     @OrderBy("id desc")
     private List<Comment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "review", cascade = {CascadeType.ALL})
+    private List<ReviewImage> reviewImages = new ArrayList<>();
 }
