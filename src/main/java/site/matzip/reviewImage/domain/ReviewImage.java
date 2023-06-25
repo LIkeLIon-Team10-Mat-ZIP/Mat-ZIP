be9f4bcef003309c3fd24951e2361ece00de8e9e
JunTaeINC <juntea3523@naver.com>
2023-06-25 15:02:45 +0900

    refactor: CommentImage -> ReviewImage 변경

--- src/main/java/site/matzip/reviewImage/domain/ReviewImage.java
+++ src/main/java/site/matzip/reviewImage/domain/ReviewImage.java
@@ -1,25 +1,26 @@
-package site.matzip.commentImage.domain;
+package site.matzip.reviewImage.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import site.matzip.review.domain.Review;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class CommentImage {
+public class ReviewImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     @NotNull
     private String imageUrl;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
 
     @Builder
-    public CommentImage(String imageUrl) {
+    public ReviewImage(String imageUrl) {
         this.imageUrl = imageUrl;
     }
 }
