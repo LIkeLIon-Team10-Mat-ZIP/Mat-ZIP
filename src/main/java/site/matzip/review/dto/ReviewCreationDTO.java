b742da36cc5cd333788007bd8d610b4de287f781
JunTaeINC <juntea3523@naver.com>
2023-06-26 22:03:16 +0900

    feat: ReviewCreationDTO author 추가

--- src/main/java/site/matzip/review/dto/ReviewCreationDTO.java
+++ src/main/java/site/matzip/review/dto/ReviewCreationDTO.java
@@ -1,10 +1,12 @@
 package site.matzip.review.dto;
 
 import lombok.Data;
+import site.matzip.member.domain.Member;
 
 @Data
 public class ReviewCreationDTO {
     private Long reviewId;
+    private Member author;
     private Long rating;
     private String content;
 }
