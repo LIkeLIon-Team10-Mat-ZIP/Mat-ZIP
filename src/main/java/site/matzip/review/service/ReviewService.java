1b58d43d0adc1ed35bcbfe07d42f98393e04518d
JunTaeINC <juntea3523@naver.com>
2023-06-28 17:24:00 +0900

    feat: review 작성 기능구현

--- src/main/java/site/matzip/review/service/ReviewService.java
+++ src/main/java/site/matzip/review/service/ReviewService.java
@@ -3,15 +3,27 @@ package site.matzip.review.service;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import site.matzip.matzip.repository.MatzipRepository;
+import site.matzip.matzip.domain.Matzip;
+import site.matzip.member.domain.Member;
 import site.matzip.review.domain.Review;
+import site.matzip.review.dto.ReviewCreationDTO;
 import site.matzip.review.repository.ReviewRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
-    private final MatzipRepository matzipRepository;
+
+    public void create(ReviewCreationDTO reviewCreationDTO, Member author, Matzip matzip) {
+        Review review = Review.builder()
+                .author(author)
+                .matzip(matzip)
+                .rating(reviewCreationDTO.getRating())
+                .content(reviewCreationDTO.getContent())
+                .build();
+
+        reviewRepository.save(review);
+    }
 
     public void remove(Long reviewId) {
         Review findReview = findReview(reviewId);
