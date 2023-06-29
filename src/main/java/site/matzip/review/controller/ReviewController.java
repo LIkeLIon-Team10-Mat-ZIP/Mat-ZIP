8802a2ce1ffed6dc29f78f39f701f65e5f1b2804
JunTae_Kim <105341433+JunTaeINC@users.noreply.github.com>
2023-06-29 14:36:58 +0900

    Matzip 엔티티 대신 DTO를 사용해 뷰로 데이터 전달 (#30)
    * feat: MatzipInfoDTO 생성
    
    * feat: MatzipInfoDTO 적용
    
    * refactor: 맛집으로 등록시 맛집리스트로 이동 (경로수정)
    
    * style: id -> matzipId

--- src/main/java/site/matzip/review/controller/ReviewController.java
+++ src/main/java/site/matzip/review/controller/ReviewController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import site.matzip.config.auth.PrincipalDetails;
 import site.matzip.matzip.domain.Matzip;
+import site.matzip.matzip.dto.MatzipInfoDTO;
 import site.matzip.matzip.service.MatzipService;
 import site.matzip.member.domain.Member;
 import site.matzip.review.domain.Review;
@@ -35,23 +36,25 @@ public class ReviewController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/create/{id}")
-    public String create(Model model, @PathVariable Long id, ReviewCreationDTO reviewCreationDTO) {
-        Matzip matzip = matzipService.findMatzip(id);
-        model.addAttribute("matzip", matzip);
+    @GetMapping("/create/{matzipId}")
+    public String create(Model model, @PathVariable Long matzipId, ReviewCreationDTO reviewCreationDTO) {
+        Matzip matzip = matzipService.findMatzip(matzipId);
+        MatzipInfoDTO matzipInfoDTO = new MatzipInfoDTO(matzip);
+
+        model.addAttribute("matzipInfoDTO", matzipInfoDTO);
         model.addAttribute("reviewCreationDTO", reviewCreationDTO);
         return "/review/add";
     }
 
     @PreAuthorize("isAuthenticated()")
-    @PostMapping("/create/{id}")
-    public String create(@PathVariable Long id, ReviewCreationDTO reviewCreationDTO,
+    @PostMapping("/create/{matzipId}")
+    public String create(@PathVariable Long matzipId, ReviewCreationDTO reviewCreationDTO,
                          BindingResult result, @AuthenticationPrincipal PrincipalDetails principalDetails) {
         if (result.hasErrors()) {
             return "/review/add";
         }
 
-        Matzip matzip = matzipService.findMatzip(id);
+        Matzip matzip = matzipService.findMatzip(matzipId);
         Member author = principalDetails.getMember();
 
         reviewService.create(reviewCreationDTO, author, matzip);
