ef1f4b5b73f5ce2ad7c2ee03ff4cb7126310d20e
Genie16-github <4411dlwoqja@naver.com>
2023-07-04 17:38:56 +0900

    feat: notification list

--- src/main/java/site/matzip/notification/controller/NotificationController.java
+++ src/main/java/site/matzip/notification/controller/NotificationController.java
@@ -1,12 +1,32 @@
 package site.matzip.notification.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import site.matzip.config.auth.PrincipalDetails;
+import site.matzip.notification.entity.Notification;
+import site.matzip.notification.service.NotificationService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/usr/notification")
 @RequiredArgsConstructor
 public class NotificationController {
+    private final NotificationService notificationService;
 
+    @GetMapping("/list")
+    @PreAuthorize("isAuthenticated()")
+    public String showList(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        List<Notification> notifications = notificationService.getNotifications(principalDetails.getMember());
+
+        model.addAttribute("notifications", notifications);
+
+        return "usr/notification/list";
+    }
 }
--- src/main/java/site/matzip/notification/service/NotificationService.java
+++ src/main/java/site/matzip/notification/service/NotificationService.java
@@ -2,9 +2,18 @@ package site.matzip.notification.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import site.matzip.member.domain.Member;
+import site.matzip.notification.entity.Notification;
+import site.matzip.notification.repository.NotificationRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
+    private final NotificationRepository notificationRepository;
 
+    public List<Notification> getNotifications(Member toMember) {
+        return notificationRepository.findByToMember(toMember);
+    }
 }
