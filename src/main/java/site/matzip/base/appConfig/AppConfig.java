89544fecde570dc4aaf7e36adafd9f1564f2c053
JunTaeINC <juntea3523@naver.com>
2023-07-04 21:55:43 +0900

    feat: AppConfig에 PointRewardReferenceTime 정의

--- src/main/java/site/matzip/base/appConfig/AppConfig.java
+++ src/main/java/site/matzip/base/appConfig/AppConfig.java
@@ -8,10 +8,17 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Getter
     private static String defaultProfileImageUrl;
+    @Getter
+    private static Long pointRewardReferenceTime;
 
     @Value("${custom.profileImage.defaultUrl}")
     public void setDefaultProfileImageUrl(String defaultProfileImageUrl) {
         AppConfig.defaultProfileImageUrl = defaultProfileImageUrl;
+    }
+
+    @Value("${custom.pointReward.referenceTime}")
+    public void setPointRewardReferenceTime(Long pointRewardReferenceTime) {
+        AppConfig.pointRewardReferenceTime = pointRewardReferenceTime;
     }
 
 }
