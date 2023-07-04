ffd4fbe59134f79afae642ccac3bb56c8281def0
JunTaeINC <juntea3523@naver.com>
2023-07-04 23:00:23 +0900

    feat: (CommentRepository) Member 객체의 프록시를 @EntityGraph 활용해 초기화

--- src/main/java/site/matzip/comment/repository/CommentRepository.java
+++ src/main/java/site/matzip/comment/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package site.matzip.comment.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @EntityGraph(attributePaths = {"author"})
     @Query("SELECT c from Comment c WHERE c.createDate < :olderThanTime")
     List<Comment> findCommentsOlderThan(@Param("olderThanTime") LocalDateTime olderThanTime);
 }
