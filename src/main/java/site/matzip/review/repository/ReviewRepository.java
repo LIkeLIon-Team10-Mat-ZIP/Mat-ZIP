46b7667cf1e81a565661e7c820714de328697d01
JunTaeINC <juntea3523@naver.com>
2023-07-05 00:24:44 +0900

    feat: (ReviewRepository) findReviewsOlderThan 구현

--- src/main/java/site/matzip/review/repository/ReviewRepository.java
+++ src/main/java/site/matzip/review/repository/ReviewRepository.java
@@ -2,9 +2,13 @@ package site.matzip.review.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import site.matzip.review.domain.Review;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -13,4 +17,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByMatzipId(Long matzipId);
 
     List<Review> findByAuthorId(Long authorId);
+
+    @EntityGraph(attributePaths = {"author"})
+    @Query("SELECT r from Review r WHERE r.createDate < :olderThanTime")
+    List<Review> findReviewsOlderThan(@Param("olderThanTime") LocalDateTime olderThanTime);
 }
