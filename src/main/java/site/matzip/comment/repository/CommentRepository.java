f55dba6f896a7723eba65a2c53cf81f33f5dbd41
JunTaeINC <juntea3523@naver.com>
2023-07-04 22:36:15 +0900

    feat: (CommentRepository) List<Comment> findCommentsOlderThan  구현

--- src/main/java/site/matzip/comment/repository/CommentRepository.java
+++ src/main/java/site/matzip/comment/repository/CommentRepository.java
@@ -1,7 +1,13 @@
 package site.matzip.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import site.matzip.comment.domain.Comment;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query("SELECT c from Comment c WHERE c.createDate < :olderThanTime")
+    List<Comment> findCommentsOlderThan(LocalDateTime olderThanTime);
 }
