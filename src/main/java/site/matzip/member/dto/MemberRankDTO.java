5aac552558f2dc5e4316b183e8174dc5d832f785
JunTaeINC <juntea3523@naver.com>
2023-07-05 23:28:08 +0900

    feat: (MemberRankDTO) badgeImage 필드 추가

--- src/main/java/site/matzip/member/dto/MemberRankDTO.java
+++ src/main/java/site/matzip/member/dto/MemberRankDTO.java
@@ -3,11 +3,13 @@ package site.matzip.member.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 @Builder
-public class MemberRankDto {
+public class MemberRankDTO {
     private String profileImageUrl;
     private String username;
-    //private String badge;
+    private Map<String, String> badgeImage;
     private long point;
 }
