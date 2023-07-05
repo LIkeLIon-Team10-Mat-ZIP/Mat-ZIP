1d7b3c7b1886f57bfddd3d3956e1ac26cc7288d4
JaeBeom <80519614+Genie16-github@users.noreply.github.com>
2023-07-05 17:29:30 +0900

    알림 기능 (#55)
    * feat: Notification Entity 생성
    
    * feat: notification list
    
    * feat: eventListener 도입
    
    * feat: 댓글 작성시 리뷰 작성자에게 알림
    
    * feat: NotificationDTO
    
    * chore: LazyLoading, @JoinColumn 사용
    
    * refactor: notification 객체 생성 과정 생성자 사용
    
    * chore: @EntityListeners(AuditingEntityListener.class) 제거

---
+++ src/main/java/site/matzip/notification/repository/NotificationRepository.java
@@ -0,0 +1,13 @@
+package site.matzip.notification.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import site.matzip.member.domain.Member;
+import site.matzip.notification.entity.Notification;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByToMember(Member toMember);
+}
