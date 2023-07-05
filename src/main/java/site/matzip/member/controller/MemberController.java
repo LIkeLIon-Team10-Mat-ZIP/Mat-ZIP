cf8bd298fb1ff9a3d010568816c1784c17e5f208
SIUUUUUUU <72070679+siwookim97@users.noreply.github.com>
2023-07-05 17:25:23 +0900

    마이페이지 뱃지 출력 (#60)
    * feat: 마이페이지를 위한 DTO 수정
    
    * feat: 뱃지 정보를 출력하도록 로직 수정
    
    * feat: 마이페이지 view 수정

--- src/main/java/site/matzip/member/controller/MemberController.java
+++ src/main/java/site/matzip/member/controller/MemberController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import site.matzip.badge.service.MemberBadgeService;
 import site.matzip.base.appConfig.AppConfig;
 import site.matzip.base.rq.Rq;
 import site.matzip.base.rsData.RsData;
@@ -34,6 +35,7 @@ public class MemberController {
 
     private final MemberService memberService;
     private final ProfileImageService profileImageService;
+    private final MemberBadgeService memberBadgeService;
     private final AppConfig appConfig;
     private final Rq rq;
 
@@ -74,6 +76,7 @@ public class MemberController {
                 .nickname(member.getNickname())
                 .email(member.getEmail())
                 .profileImageUrl(profileImageUrl)
+                .badgeImage(memberBadgeService.showMemberBadge(member))
                 .build();
 
         model.addAttribute("memberInfoDTO", memberInfoDTO);
