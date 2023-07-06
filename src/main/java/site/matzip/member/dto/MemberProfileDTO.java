ecafcf0e4fdbb69c3ace7f30175d8c90afb2907b
JunTaeINC <juntea3523@naver.com>
2023-07-06 17:12:44 +0900

    feat: (MemberProfileDTO) 필드 수정

--- src/main/java/site/matzip/member/dto/MemberProfileDTO.java
+++ src/main/java/site/matzip/member/dto/MemberProfileDTO.java
@@ -3,21 +3,13 @@ package site.matzip.member.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @Data
 @Builder
 public class MemberProfileDTO {
     private String profileImageUrl;
     private String nickname;
-    private LocalDateTime createDate;
     private long matzipCount;
     private long reviewCount;
     private long point;
 
-    public String getFormattedCreateDate() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        return createDate.format(formatter);
-    }
 }
