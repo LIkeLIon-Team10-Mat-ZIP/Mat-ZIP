9f21d4813e8ad29c84a42f60e21b82a987d61dab
JunTaeINC <juntea3523@naver.com>
2023-07-11 22:01:09 +0900

    feat: (MatzipRankDTO) DTO 생성

---
+++ src/main/java/site/matzip/matzip/dto/MatzipRankDTO.java
@@ -0,0 +1,13 @@
+package site.matzip.matzip.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MatzipRankDTO {
+    private String matzipName;
+    private double averageRating;
+    private long userCount;
+    private long reviewCount;
+}
