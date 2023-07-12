c2367e357f6a773ae2d5a3c994cce149ca1a8245
JunTaeINC <juntea3523@naver.com>
2023-07-12 20:11:57 +0900

    feat: (ReviewCreationDTO) 어노테이션 추가

--- src/main/java/site/matzip/review/dto/ReviewCreationDTO.java
+++ src/main/java/site/matzip/review/dto/ReviewCreationDTO.java
@@ -2,13 +2,19 @@ package site.matzip.review.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewCreationDTO {
     @NotBlank(message = "리뷰점수를 입력해주세요")
     private double rating;
