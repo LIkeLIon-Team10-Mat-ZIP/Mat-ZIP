014bb5d14bfe35dc1b6f3474b3e115996b1d4e16
siwookim97 <tldn12gh@gmail.com>
2023-07-13 16:59:19 +0900

    !hotfix: 친구 연관관계 수정

--- src/main/java/site/matzip/friend/domain/Friend.java
+++ src/main/java/site/matzip/friend/domain/Friend.java
@@ -6,26 +6,48 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import site.matzip.base.domain.BaseEntity;
 import site.matzip.member.domain.Member;
+import site.matzip.review.domain.Review;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Friend extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member1_id")
-
     private Member member1;
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member2_id")
     private Member member2;
 
-    public Friend(Member member1, Member member2) {
+//    public Friend(Member member1, Member member2) {
+//        this.member1 = member1;
+//        this.member2 = member2;
+//    }
+
+    public void addAssociation(Member member1, Member member2) {
+        addMember1(member1);
+        addMember2(member2);
+    }
+
+    private void addMember1(Member member1) {
+        if (this.member1 != null) {
+            this.member1.getFriends1().remove(this);
+        }
         this.member1 = member1;
+        member1.getFriends1().add(this);
+    }
+
+    private void addMember2(Member member2) {
+        if (this.member2 != null) {
+            this.member2.getFriends2().remove(this);
+        }
         this.member2 = member2;
+        member2.getFriends2().add(this);
     }
 }
 
