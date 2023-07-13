e677530f8f4703bb87d579893a1adeb9862fec60
JunTaeINC <juntea3523@naver.com>
2023-07-13 10:22:15 +0900

    feat: (MatzipCreationDTO) Builder 추가

--- src/main/java/site/matzip/matzip/dto/MatzipCreationDTO.java
+++ src/main/java/site/matzip/matzip/dto/MatzipCreationDTO.java
@@ -2,14 +2,12 @@ package site.matzip.matzip.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import site.matzip.matzip.domain.MatzipType;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MatzipCreationDTO {
