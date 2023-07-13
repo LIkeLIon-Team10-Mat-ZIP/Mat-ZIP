b84e39a971a8bed7e1a40e48ccd259c45228f748
siwookim97 <tldn12gh@gmail.com>
2023-07-13 17:10:59 +0900

    !HOTFIX: Friend 엔티티 연관관계 매핑

--- src/main/java/site/matzip/friend/domain/Friend.java
+++ src/main/java/site/matzip/friend/domain/Friend.java
@@ -9,23 +9,39 @@ import site.matzip.member.domain.Member;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Friend extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member1_id")
 
     private Member member1;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member2_id")
     private Member member2;
 
-    public Friend(Member member1, Member member2) {
+    public void addAssociation(Member member1, Member member2) {
+        addMember1(member1);
+        addMember2(member2);
+    }
+
+    private void addMember1(Member member1) {
+        if (this.member1 != null) {
+            this.member1.getFriends1().remove(this);
+        }
         this.member1 = member1;
+        member1.getFriends1().add(this);
+    }
+
+    private void addMember2(Member member2) {
+        if (this.member2 != null) {
+            this.member2.getFriends2().remove(this);
+        }
         this.member2 = member2;
+        member2.getFriends2().add(this);
     }
 }
 
