3bcef4412c13f0654e50b57abd77586bdae3a732
siwookim97 <tldn12gh@gmail.com>
2023-10-27 23:54:49 +0900

    fix: Badge 부여 쿼리 10분마다로 스케줄 변경

--- src/main/java/site/matzip/member/repository/MemberRepository.java
+++ src/main/java/site/matzip/member/repository/MemberRepository.java
@@ -15,22 +15,22 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByUsername(String username);
 
+    @Query("SELECT m FROM Member m JOIN FETCH m.reviews")
+    List<Member> findAllWithReviews();
+
     List<Member> findTop10ByOrderByPointDesc();
 
     List<Member> findAllByOrderByPointDesc();
 
-    @Query("SELECT m FROM Member m JOIN FETCH m.matzipMembers matzip WHERE SIZE(matzip) > :count")
+    @Query("SELECT m FROM Member m JOIN FETCH m.matzipMembers matzip WHERE SIZE(matzip) >= :count")
     List<Member> findMembersWithMatzipCountGreaterThan(@Param("count") int count);
 
-    @Query("SELECT m FROM Member m JOIN FETCH m.reviews r WHERE SIZE(m.reviews) > :count")
+    @Query("SELECT m FROM Member m JOIN FETCH m.reviews r WHERE SIZE(m.reviews) >= :count")
     List<Member> findMembersWithReviewsAndCountGreaterThan(@Param("count") int count);
 
-    @Query("SELECT m FROM Member m JOIN FETCH m.comments c WHERE SIZE(m.comments) > :count")
+    @Query("SELECT m FROM Member m JOIN FETCH m.comments c WHERE SIZE(m.comments) >= :count")
     List<Member> findMembersWithCommentsAndCountGreaterThan(@Param("count") int count);
 
-    @Query("SELECT m FROM Member m JOIN FETCH m.friends2 f WHERE SIZE(f.friends2) > :count")
+    @Query("SELECT m FROM Member m JOIN FETCH m.friends2 f WHERE SIZE(m.friends2) >= :count")
     List<Member> findMembersWithFriends2AndCountGreaterThan(@Param("count") int count);
-
-    @Query("SELECT m FROM Member m JOIN FETCH m.reviews")
-    List<Member> findAllWithReviews();
 }
